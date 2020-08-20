dd1b7e9e1a284f058d5a793961e6fcfc1554d021
Keith Nolan <g00351932@gmit.ie>
2020-08-20 18:25:29 +0100

    Getting started on front ned displayimg data to screen from api

--- covid_19-tracker/src/components/Cards/Cards.jsx
+++ covid_19-tracker/src/components/Cards/Cards.jsx
@@ -1,9 +1,55 @@
 import React from 'react';
+import {Card, CardContent, Typography, Grid} from '@material-ui/core';
 
-const Cards = (props) => {
-    console.log(props)
+import styles from './Cards.module.css';
+import CountUp from 'react-countup';
+
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+   if(!confirmed){
+       return "Loading...";
+
+   }
     return(
-        <h1>Cards</h1>
+        <div className = {styles.container}>
+        <Grid container spacing={3} justify = "center">
+            <Grid item component={Card}>
+            <CardContent>
+                <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                <Typography varient = "h5">
+                    <CountUp
+                    start={0}
+                    end={confirmed.value}
+                    duration={2.5}
+                    separator=","
+                    />
+                </Typography>
+                <Typography color= "textSecondary">REAL DATE</Typography>
+                <Typography varient = "body2">Number of active cases of COVID-19</Typography>
+            </CardContent>
+
+            </Grid>
+            <Grid item component={Card}>
+            <CardContent>
+                <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+                <Typography varient = "h5">REAL DATA </Typography>
+                <Typography color= "textSecondary">REAL DATE</Typography>
+                <Typography varient = "body2">Number of recoveries from COVID-19</Typography>
+            </CardContent>
+
+            </Grid>
+            <Grid item component={Card}>
+            <CardContent>
+                <Typography color="textSecondary" gutterBottom>Infected</Typography>
+                <Typography varient = "h5">REAL DATA </Typography>
+                <Typography color= "textSecondary">REAL DATE</Typography>
+                <Typography varient = "body2">Number of deaths from COVID-19</Typography>
+            </CardContent>
+
+            </Grid>
+        </Grid>
+        </div>
+
+       
     )
 }
 
